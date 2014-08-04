f9556238317e035f77cc77eca9d26616b053b662
mhawila <a.mhawila@gmail.com>
2014-08-04 15:33:48 -0400

    ONC-11: Disabled login interceptor & Modified code to implement and use case insensitive search of roles

--- omod/src/main/java/org/openmrs/module/oncologypoc/web/controller/LoginInterceptController.java
+++ omod/src/main/java/org/openmrs/module/oncologypoc/web/controller/LoginInterceptController.java
@@ -6,31 +6,31 @@ import org.openmrs.module.oncologypoc.api.utils.OncologyPOCConstants;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+//@Controller
 public class LoginInterceptController {
-	@RequestMapping({ "/index.htm" })
-	public String redirectIntercepted() {
-		User autheticatedUser = Context.getAuthenticatedUser();
-
-		if ((autheticatedUser != null) && (!autheticatedUser.isSuperUser())) {
-			String prop = Context.getAdministrationService().getGlobalProperty(OncologyPOCConstants.GP_INTERCEPTROLES);
-
-			if ((prop != null) && (prop.length() > 0)) {
-				String[] interceptRoles = prop.split(",");
-				boolean interceptThisOne = false;
-
-				for (String role : interceptRoles) {
-					if (autheticatedUser.hasRole(role.trim())){
-						interceptThisOne = true;
-						break;
-					}
-				}
-
-				if (interceptThisOne) {
-					return "redirect:/module/oncologypoc/patientSearch.htm";
-				}
-			}
-		}
-		return null;
-	}
+//	@RequestMapping({ "/index.htm" })
+//	public String redirectIntercepted() {
+//		User autheticatedUser = Context.getAuthenticatedUser();
+//
+//		if ((autheticatedUser != null) && (!autheticatedUser.isSuperUser())) {
+//			String prop = Context.getAdministrationService().getGlobalProperty(OncologyPOCConstants.GP_INTERCEPTROLES);
+//
+//			if ((prop != null) && (prop.length() > 0)) {
+//				String[] interceptRoles = prop.split(",");
+//				boolean interceptThisOne = false;
+//
+//				for (String role : interceptRoles) {
+//					if (autheticatedUser.hasRole(role.trim())){
+//						interceptThisOne = true;
+//						break;
+//					}
+//				}
+//
+//				if (interceptThisOne) {
+//					return "redirect:/module/oncologypoc/patientSearch.htm";
+//				}
+//			}
+//		}
+//		return null;
+//	}
 }
--- omod/src/main/java/org/openmrs/module/oncologypoc/web/filter/OncologyPOCFilter.java
+++ omod/src/main/java/org/openmrs/module/oncologypoc/web/filter/OncologyPOCFilter.java
@@ -1,6 +1,7 @@
 package org.openmrs.module.oncologypoc.web.filter;
 
 import java.io.IOException;
+import java.util.Set;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Role;
 import org.openmrs.User;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.oncologypoc.api.utils.OncologyPOCConstants;
@@ -25,40 +27,49 @@ public class OncologyPOCFilter implements Filter {
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		User autheticatedUser = Context.getAuthenticatedUser();
+		User authenticatedUser = Context.getAuthenticatedUser();
 
-		if (autheticatedUser != null) {
+		if (authenticatedUser != null) {
 			String prop = Context.getAdministrationService().getGlobalProperty(OncologyPOCConstants.GP_INTERCEPTROLES);
 			String requestURL = ((HttpServletRequest) request).getRequestURI();
 			String patientId = ((HttpServletRequest) request).getParameter("patientId");
-			this.log.debug("OncologyPOCFilter.doFilter: " + requestURL + "|" + patientId);
+			log.debug("OncologyPOCFilter.doFilter: " + requestURL + "|" + patientId);
 
 			if ((prop != null) && (prop.length() > 0)) {
 				String[] interceptRoles = prop.split(",");
 				boolean interceptThisOne = false;
-
 				for (String role : interceptRoles) {
-					if (autheticatedUser.hasRole(role.trim())){
+					if (hasRole(authenticatedUser,role.trim())){
 						interceptThisOne = true;
 						break;
 					}
 				}
 
-				if (interceptThisOne && !autheticatedUser.isSuperUser()) {
+				if (interceptThisOne && !authenticatedUser.isSuperUser()) {
 					if (requestURL.contains("patientDashboard"))
 						((HttpServletResponse) response).sendRedirect(((HttpServletRequest) request).getContextPath() + homePage + "?patientId=" + patientId);
 					if (requestURL.contains("findPatient.htm"))
 						((HttpServletResponse) response).sendRedirect(((HttpServletRequest) request).getContextPath() + "/module/oncologypoc/patientSearch.htm");
 					//if (requestURL.contains("index.htm"))
 						//((HttpServletResponse) response).sendRedirect(((HttpServletRequest) request).getContextPath() + "/module/oncologypoc/patientSearch.htm");
-				} else
+				} else {
 					chain.doFilter(request, response);
+                }
 			}
 		}
-
 	}
 
 	public void init(FilterConfig config) throws ServletException {
 		homePage = config.getInitParameter("oncologyHomePage");
 	}
+
+    //Custom case insensitive hasRole method
+    private static  boolean hasRole(final User user,final String roleName) {
+        if(user==null || roleName==null || roleName.isEmpty())return false;
+        Set<Role> roles = user.getAllRoles();
+        for(Role role:user.getAllRoles()) {
+             if(role.getName().equalsIgnoreCase(roleName))return true;
+        }
+        return false;
+    }
 }
